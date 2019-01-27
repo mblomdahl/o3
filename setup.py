29e0b4c17290e88e97699bcbccd5d798d5c4bf68
Mats Blomdahl <mats.blomdahl@gmail.com>
2019-01-27 16:25:49 +0100

    #13 Add setup.py and package structure to make custom hooks/sensors/operator importable

---
+++ setup.py
@@ -0,0 +1,18 @@
+#  -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='o3',
+    packages=find_packages(include=('o3',)),
+    url='https://github.com/mblomdahl/o3',
+    license='The Unlicense',
+    description='Hadoop-Airflow analytics',
+    # https://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.6'
+    ],
+    platforms=['POSIX']
+)
