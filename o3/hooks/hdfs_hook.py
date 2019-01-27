485903480dd28246540f4c6076bcee9e419562e9
Mats Blomdahl <mats.blomdahl@gmail.com>
2019-01-27 16:20:43 +0100

    #13 Use HDFS3 without PyArrow wrapper (causing Segmentation Faults...)

--- o3/hooks/hdfs_hook.py
+++ o3/hooks/hdfs_hook.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 """
 Copy of :py:mod:`airflow.hooks.hdfs_hook`, modified to replace Py3 incompatible
-snakebite library wrapping with PyArrow HDFS driver.
+snakebite library wrapping with libhdfs3 driver.
 """
 
 from airflow.hooks.base_hook import BaseHook
 
-import pyarrow
+import hdfs3
 
 
 # noinspection PyAbstractClass
 class HDFSHook(BaseHook):
-    """Interact with HDFS. Wrapper around the PyArrow HadoopFileSystem.
+    """Interact with HDFS. Wrapper around the hdfs3.HDFileSystem.
 
     :param str hdfs_conn_id: Connection ID to fetch connection info
     :param str effective_user: Effective user for HDFS operations
@@ -23,7 +23,7 @@ class HDFSHook(BaseHook):
         self.hdfs_conn_id = hdfs_conn_id
         self.effective_user = effective_user
 
-    def get_conn(self) -> pyarrow.hdfs.HadoopFileSystem:
+    def get_conn(self) -> hdfs3.core.HDFileSystem:
 
         effective_user = self.effective_user
 
@@ -32,5 +32,5 @@ class HDFSHook(BaseHook):
         if not self.effective_user:
             effective_user = connection.login
 
-        return pyarrow.hdfs.connect(host=connection.host, port=connection.port,
-                                    user=effective_user, driver='libhdfs3')
+        return hdfs3.HDFileSystem(host=connection.host, port=connection.port,
+                                  user=effective_user)
