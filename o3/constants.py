f8009e57b4a74b80bc391f2c519ad7d390a9192a
Mats Blomdahl <mats.blomdahl@gmail.com>
2019-02-08 20:17:35 +0100

    Apply best practices from Ivan Celikovic! :)

--- o3/constants.py
+++ o3/constants.py
@@ -1,8 +1,26 @@
 """Importable constants for `o3` project."""
 
+from datetime import datetime, timedelta
+
 LOG_CLASSIFIERS = [
     'Location', 'Performance', 'BSS', 'BSSEnd', 'AppInfo', 'Connection',
     'Disconnect', 'RxTxData', 'Failure', 'SystemInfo', 'NWDOperation',
     'KPIInfo', 'UserAction', 'QoS', 'RadioManagement',
     'RadioManagementEnd', 'ThroughputSamples', 'CaptivePortalAlert'
 ]
+
+
+DEFAULT_ARGS = {
+    'owner': 'mblomdahl',
+    'depends_on_past': False,
+    'start_date': datetime(2019, 1, 18),
+    'email': ['mats.blomdahl@gmail.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 3,
+    'retry_delay': timedelta(seconds=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+}
