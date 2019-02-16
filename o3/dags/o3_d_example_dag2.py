e955a2933c3c2bfa94ab59f86f40904ffd630441
Mats Blomdahl <mats.blomdahl@gmail.com>
2019-02-16 17:15:38 +0100

    Name example dags for what they are, e.g. `o3_d_example_dag4.py` instead of `dag4.py`

--- o3/dags/o3_d_example_dag2.py
+++ o3/dags/o3_d_example_dag2.py
@@ -1,25 +1,31 @@
+# -*- coding: utf-8 -*-
 """
-### A stab at https://github.com/mblomdahl/o3/issues/10
+### A stab at example DAG 2 (https://github.com/mblomdahl/o3/issues/13)
 
-Every hour:
+Preparation:
 
-* Waits for something to show up in `$AIRFLOW_HOME/input/` dir
-* Moves *.txt files in input dir to `$AIRFLOW_HOME/processing/`
+* Go to web UI, _Admin_ > _Connections_ > _Create_
+* Configure new HDFS type connection with ID `hdfs_default`
+* Save it and trigger `o3_d_dag2` manually
+
+What it does:
+
+* Ensures the necessary directories exists in HDFS
+* Waits for something to show up in HDFS `/user/airflow/input/` dir
+* Moves *.txt files in input dir to HDFS `/user/airflow/processing/`
 * Primitive word and row counts in 2 parallel operations
 * Prints a summary of word/row counts
 * Deletes the processed input file
 
-Totally awesome, right!
 
 """
 
 from os import path
-from datetime import timedelta
 
-from airflow import DAG, conf
-from airflow.contrib.sensors.file_sensor import FileSensor
+from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 
+from o3.sensors.hdfs_sensor import HDFSSensor
 from o3.operators.ensure_dir_operator import EnsureDirOperator
 from o3.operators.move_file_operator import MoveFileOperator
 from o3.operators.word_count_operator import WordCountOperator
@@ -29,9 +35,9 @@ from o3.operators.remove_file_operator import RemoveFileOperator
 from o3.constants import DEFAULT_ARGS
 
 
-FILE_INPUT_DIR = path.join(conf.AIRFLOW_HOME, 'input')
+FILE_INPUT_DIR = path.join('/user/airflow/', 'input')
 
-PROCESSING_DIR = path.join(conf.AIRFLOW_HOME, 'processing')
+PROCESSING_DIR = path.join('/user/airflow/', 'processing')
 
 
 def _get_input_filepath(**ctx):
@@ -44,33 +50,33 @@ def _summarize_counts(**ctx):
     return f'summary: {words} / {rows}'
 
 
-with DAG('o3_d_dag1', default_args=DEFAULT_ARGS,
-         schedule_interval=timedelta(minutes=60), catchup=False) as _dag:
+with DAG('o3_d_example_dag2', default_args=DEFAULT_ARGS,
+         schedule_interval='@once', catchup=False) as _dag:
 
     ensure_dirs = EnsureDirOperator(task_id='o3_t_ensure_dirs_exist',
                                     paths=[FILE_INPUT_DIR, PROCESSING_DIR],
-                                    fs_type='local')
+                                    fs_type='hdfs')
 
-    scan_input_dir = FileSensor(task_id='o3_s_scan_input_dir',
-                                fs_conn_id='fs_default',
+    scan_input_dir = HDFSSensor(task_id='o3_s_scan_input_dir',
+                                hdfs_conn_id='hdfs_default',
                                 filepath=FILE_INPUT_DIR)
 
     move_input_file = MoveFileOperator(task_id='o3_t_get_input_file',
                                        glob_pattern='*.txt',
                                        src_dir=FILE_INPUT_DIR,
                                        dest_dir=PROCESSING_DIR,
-                                       src_fs_type='local',
-                                       dest_fs_type='local',
+                                       src_fs_type='hdfs',
+                                       dest_fs_type='hdfs',
                                        max_files=1,
                                        depends_on_past=True)
 
     count_words = WordCountOperator(task_id='o3_t_count_words',
                                     filepath=_get_input_filepath,
-                                    fs_type='local')
+                                    fs_type='hdfs')
 
     count_rows = RowCountOperator(task_id='o3_t_count_rows',
                                   filepath=_get_input_filepath,
-                                  fs_type='local')
+                                  fs_type='hdfs')
 
     summarize_counts = PythonOperator(task_id='o3_t_summarize',
                                       python_callable=_summarize_counts,
@@ -78,7 +84,7 @@ with DAG('o3_d_dag1', default_args=DEFAULT_ARGS,
 
     remove_input_file = RemoveFileOperator(task_id='o3_t_remove_input',
                                            filepath=_get_input_filepath,
-                                           fs_type='local')
+                                           fs_type='hdfs')
 
     # Workflow!
     ensure_dirs >> scan_input_dir >> move_input_file >> \
