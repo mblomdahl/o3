10a041a6e37407cdbb3f5083eae55c494b099aa4
Mats Blomdahl <mats.blomdahl@gmail.com>
2019-02-24 13:05:55 +0100

    Refactor `IngestAvroIntoHiveOperator` to support loading a list of avro files into a single table

--- o3/operators/ingest_avro_into_hive_operator.py
+++ o3/operators/ingest_avro_into_hive_operator.py
@@ -52,62 +52,70 @@ class IngestAvroIntoHiveOperator(BaseOperator):
         self.hdfs_processing_dir = hdfs_processing_dir.rstrip('/')
         self.remove_src = remove_src
 
-    def execute(self, context) -> None:
-        target_table = self.target_table
-        avro_schema_path = self.avro_schema_path
-        src_filepath = self.src_filepath_str or self.src_filepath_callable(
-            **context)
-        tmp_avro_path = os.path.join(self.hdfs_processing_dir,
-                                     os.path.basename(src_filepath))
-
+    def _move_src_files_to_hdfs(self, src_filepaths: list) -> list:
+        temp_avro_paths = []
         hdfs = HDFSHook().get_conn()
 
-        if not hdfs.exists(avro_schema_path):
-            raise AirflowException(f'Avro schema {avro_schema_path} not found.')
-        full_schema_path = f'hdfs://{hdfs.host}:{hdfs.port}{avro_schema_path}'
+        for src_filepath in src_filepaths:
+            temp_avro_path = os.path.join(self.hdfs_processing_dir,
+                                          os.path.basename(src_filepath))
+            if not hdfs.exists(temp_avro_path):
+                self.log.info(f'Moving local file {src_filepath} to '
+                              f'HDFS {temp_avro_path}')
+                hdfs.put(src_filepath, temp_avro_path, replication=1)
+                if self.remove_src:
+                    self.log.info(f'Removing {src_filepath}.')
+                    os.remove(src_filepath)
+            temp_avro_paths.append(temp_avro_path)
 
-        if not hdfs.exists(tmp_avro_path):
-            self.log.info(f'Moving local file {src_filepath} to '
-                          f'HDFS {tmp_avro_path}')
-            hdfs.put(src_filepath, tmp_avro_path, replication=1)
+        return temp_avro_paths
 
-            if self.remove_src:
-                self.log.info(f'Removing {src_filepath}.')
-                os.remove(src_filepath)
-
+    @staticmethod
+    def _load_avro_into_temp_tables(temp_avro_paths: list,
+                                    full_schema_path: str) -> list:
+        temp_table_names = []
         conn = PyHiveHook().get_conn()
-
         cursor = conn.cursor()
 
-        temp_table_name = os.path.basename(src_filepath).replace(
-            '.', '_').replace('-', '_')
+        for temp_avro_path in temp_avro_paths:
+            temp_table_name = os.path.basename(temp_avro_path).replace(
+                '.', '_').replace('-', '_')
 
-        create_temp_table_stmt = f"""
-            CREATE TABLE IF NOT EXISTS {temp_table_name}
-            ROW FORMAT SERDE '{ROW_FORMAT}'
-            STORED AS INPUTFORMAT '{INPUT_FMT}'
-            OUTPUTFORMAT '{OUTPUT_FMT}'
-            TBLPROPERTIES ('avro.schema.url'='{full_schema_path}')
-        """
-        print(f'--- create_temp_table_stmt ---\n{create_temp_table_stmt}')
-        cursor.execute(create_temp_table_stmt)
-
-        select_temp_row_stmt = f"""
-            SELECT * FROM {temp_table_name} LIMIT 1
-        """
-        print(f'--- select_temp_row_stmt---\n{select_temp_row_stmt}')
-        cursor.execute(select_temp_row_stmt)
-
-        if cursor.fetchone() is None:
-            load_data_stmt = f"""
-                LOAD DATA INPATH '{tmp_avro_path}'
-                INTO TABLE {temp_table_name}
+            create_temp_table_stmt = f"""
+                CREATE TABLE IF NOT EXISTS {temp_table_name}
+                ROW FORMAT SERDE '{ROW_FORMAT}'
+                STORED AS INPUTFORMAT '{INPUT_FMT}'
+                OUTPUTFORMAT '{OUTPUT_FMT}'
+                TBLPROPERTIES ('avro.schema.url'='{full_schema_path}')
             """
-            print(f'--- load_data_stmt ---\n{load_data_stmt}')
-            cursor.execute(load_data_stmt)
+            print(f'--- create_temp_table_stmt ---\n{create_temp_table_stmt}')
+            cursor.execute(create_temp_table_stmt)
+
+            select_temp_row_stmt = f"""
+                SELECT * FROM {temp_table_name} LIMIT 1
+            """
+            print(f'--- select_temp_row_stmt---\n{select_temp_row_stmt}')
+            cursor.execute(select_temp_row_stmt)
+
+            if cursor.fetchone() is None:
+                load_data_stmt = f"""
+                    LOAD DATA INPATH '{temp_avro_path}'
+                    INTO TABLE {temp_table_name}
+                """
+                print(f'--- load_data_stmt ---\n{load_data_stmt}')
+                cursor.execute(load_data_stmt)
+
+            temp_table_names.append(temp_table_name)
+
+        return temp_table_names
+
+    def _insert_temp_data_into_target_table(self, temp_table_names: list,
+                                            full_schema_path: str) -> None:
+        conn = PyHiveHook().get_conn()
+        cursor = conn.cursor()
 
         create_target_table_stmt = f"""
-            CREATE EXTERNAL TABLE IF NOT EXISTS {target_table}
+            CREATE EXTERNAL TABLE IF NOT EXISTS {self.target_table}
             PARTITIONED BY (ds STRING, h STRING, en STRING)
             ROW FORMAT SERDE '{ROW_FORMAT}'
             STORED AS INPUTFORMAT '{INPUT_FMT}'
@@ -117,21 +125,50 @@ class IngestAvroIntoHiveOperator(BaseOperator):
         print(f'--- create_target_table_stmt ---\n{create_target_table_stmt}')
         cursor.execute(create_target_table_stmt)
 
-        insert_data_stmt = f"""
-            INSERT INTO {target_table} PARTITION (ds, h, en)
-            SELECT
-                *,
-                datestamp AS ds,
-                substr(server_date, 12, 2) AS h,
-                event_name AS en
-            FROM
-                {temp_table_name}
-        """
-        print(f'--- insert_data_stmt ---\n{insert_data_stmt}')
-        cursor.execute(insert_data_stmt)
+        for temp_table_name in temp_table_names:
+            insert_data_stmt = f"""
+                INSERT INTO {self.target_table} PARTITION (ds, h, en)
+                SELECT
+                    *,
+                    datestamp AS ds,
+                    substr(server_date, 12, 2) AS h,
+                    event_name AS en
+                FROM
+                    {temp_table_name}
+            """
+            print(f'--- insert_data_stmt ---\n{insert_data_stmt}')
+            cursor.execute(insert_data_stmt)
 
-        drop_temp_table_stmt = f"""
-            DROP TABLE {temp_table_name}
-        """
-        print(f'--- drop_temp_table_stmt ---\n{drop_temp_table_stmt}')
-        cursor.execute(drop_temp_table_stmt)
+            drop_temp_table_stmt = f"""
+                DROP TABLE {temp_table_name}
+            """
+            print(f'--- drop_temp_table_stmt ---\n{drop_temp_table_stmt}')
+            cursor.execute(drop_temp_table_stmt)
+
+    def execute(self, context) -> None:
+        avro_schema_path = self.avro_schema_path
+        if self.src_filepath_callable:
+            src_filepath = self.src_filepath_callable(**context)
+        else:
+            try:
+                src_filepath = eval(self.src_filepath_str)
+            except SyntaxError:
+                src_filepath = self.src_filepath_str
+
+        if isinstance(src_filepath, list):
+            src_filepaths = src_filepath
+        else:
+            src_filepaths = [src_filepath]
+
+        hdfs = HDFSHook().get_conn()
+        if not hdfs.exists(avro_schema_path):
+            raise AirflowException(f'Avro schema {avro_schema_path} not found.')
+        full_schema_path = f'hdfs://{hdfs.host}:{hdfs.port}{avro_schema_path}'
+
+        temp_avro_paths = self._move_src_files_to_hdfs(src_filepaths)
+
+        temp_table_names = self._load_avro_into_temp_tables(temp_avro_paths,
+                                                            full_schema_path)
+
+        self._insert_temp_data_into_target_table(temp_table_names,
+                                                 full_schema_path)
