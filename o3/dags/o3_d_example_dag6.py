064d6e75b95eb8cdb198fe8924f9b0e9e550157a
Mats Blomdahl <mats.blomdahl@gmail.com>
2019-03-02 17:00:32 +0100

    Finishing touches for `o3_d_example_dag6`

--- o3/dags/o3_d_example_dag6.py
+++ o3/dags/o3_d_example_dag6.py
@@ -1,5 +1,5 @@
 """
-### `o3_d_example_dag6` -- A DAG to Fail and Send an Email About It
+### `o3_d_example_dag6` -- A DAG to Fail, Retry, and Send Email Alerts About It
 
 
 """
@@ -14,7 +14,7 @@ from o3.constants import DEFAULT_ARGS
 
 with DAG('o3_d_example_dag6',
          default_args=DEFAULT_ARGS,
-         schedule_interval=None,
+         schedule_interval="*/5 * * * *",
          catchup=False) as _dag:
     send_email_about_starting_job = EmailOperator(
         task_id='o3_t_email_about_starting_job',
@@ -29,7 +29,16 @@ with DAG('o3_d_example_dag6',
         bash_command='sleep 30'
     )
 
-    send_email_about_starting_job >> break_sla
+    fail_task_and_break_sla = BashOperator(
+        task_id='o3_t_fail_task_and_break_sla',
+        sla=timedelta(seconds=4),
+        bash_command='sleep 5 && false',
+        retries=3,
+        email_on_retry=True,
+        email_on_failure=True
+    )
+
+    send_email_about_starting_job >> break_sla >> fail_task_and_break_sla
 
 
 _dag.doc_md = __doc__
